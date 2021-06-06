7b6dd62061c9d26835037134602c4c5937da3c4a
Pedro jr <pedro81565@hotmail.com.com>
2021-06-06 15:32:26 -0300

    User entity and resource

---
+++ src/main/java/com/smart/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.smart.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smart.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Pedro", "Pedro@teste.com", "99999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
