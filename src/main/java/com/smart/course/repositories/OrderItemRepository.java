f7cc08b22181014987874d9966dac1ffe37a3268
Pedro jr <pedro81565@hotmail.com.com>
2021-06-07 14:11:39 -0300

    association with extra attributes pt 2

---
+++ src/main/java/com/smart/course/repositories/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package com.smart.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smart.course.entities.OrderItem;
+
+
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+	
+
+}
