117f9769fd803146d50d41d1686652aeb81bdb4e
Pedro jr <pedro81565@hotmail.com.com>
2021-06-07 12:35:20 -0300

    Product

---
+++ src/main/java/com/smart/course/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.smart.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smart.course.entities.Product;
+
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	
+
+}
---
+++ src/main/java/com/smart/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.smart.course.services;
+
+import com.smart.course.entities.Product;
+import com.smart.course.repositories.ProductRepository;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
